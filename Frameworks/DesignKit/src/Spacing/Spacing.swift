34f5a79996cb2001066e7fcba3fa8e3a44453e56
diaojiagou <xcodeman@163.com>
2022-03-12 01:36:40 +0800

    增加 内部公共组件库

---
+++ Frameworks/DesignKit/src/Spacing/Spacing.swift
@@ -0,0 +1,18 @@
+//
+//  Spacing.swift
+//  
+//
+//  Created by 刁爷 on 2022/3/11.
+//
+
+import Foundation
+
+public struct Spacing {
+    public static let twoExtraSmall: CGFloat = 4
+    public static let extraSmall: CGFloat = 8
+    public static let small: CGFloat = 12
+    public static let medium: CGFloat = 18
+    public static let large: CGFloat = 24
+    public static let extraLarge: CGFloat = 40
+    public static let twoExtraLarge: CGFloat = 48
+}
---
+++ Frameworks/DesignKit/src/UIFontExtensions/UIFontExtensions.swift
@@ -0,0 +1,10 @@
+//
+//  Spacing.swift
+//  
+//
+//  Created by 刁爷 on 2022/3/11.
+//
+
+import UIKit
+
+
