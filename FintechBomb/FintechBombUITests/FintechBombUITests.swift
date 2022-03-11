e7187c097b624b6bffdfbe1398f9db1dbf798c45
diaojiagou <xcodeman@163.com>
2022-03-11 22:57:37 +0800

    增加 swiftlint 统一代码规范

--- FintechBomb/FintechBombUITests/FintechBombUITests.swift
+++ FintechBomb/FintechBombUITests/FintechBombUITests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 class FintechBombUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
